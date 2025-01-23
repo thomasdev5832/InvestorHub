3415908fc9c971d6c24017a80c7634d235f84c2c
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2025-01-23 20:08:13 -0300

    feat: implementation of new class that start dynamically all client and contracts to be listening

---
+++ staking-aggregator-service/src/main/kotlin/io/sevenseven/staking/aggregator/service/resource/blockchain/uniswap/EthLogFlowableListener.kt
@@ -0,0 +1,88 @@
+package io.sevenseven.staking.aggregator.service.resource.blockchain.uniswap
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.reactivex.Flowable
+import io.sevenseven.staking.aggregator.service.application.config.BlockchainNodeConfig
+import jakarta.annotation.PostConstruct
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.protocol.core.methods.request.EthFilter
+import org.web3j.protocol.core.methods.response.Log
+
+// https://github.com/alainncls/web3j-spring-boot-example/blob/main/src/main/java/com/consensys/web3j/Web3jApplication.java
+@Component
+class EthLogFlowableListener(
+    private val blockchainNodeConfig: BlockchainNodeConfig,
+    private val objectMapper: ObjectMapper
+) {
+    private val logger: Logger = LoggerFactory.getLogger(EthLogFlowableListener::class.java)
+
+    @PostConstruct
+    fun init() {
+        logger.info("Starting EthLogFlowableListener...")
+
+        startListenToEvents()
+    }
+
+    private fun startListenToEvents() {
+        blockchainNodeConfig.nodes.forEach {
+            startClient(
+                client = it.web3j(),
+                contracts = it.pools
+            )
+        }
+    }
+
+    @Async
+    private fun startClient(client: Web3j, contracts: List<String>) =
+        client.let {
+            contracts.forEach { contractAddress ->
+                Contract(
+                    it,
+                    contractAddress,
+                    objectMapper
+                ).let { contract ->
+                    subscribeToEvents(contract)
+                }
+            }
+        }
+
+    @Async
+    private fun subscribeToEvents(contract: Contract) =
+        contract.eventFlowable().subscribe { event ->
+            logger.info("Listening to events...")
+        }
+}
+
+class Contract(
+    private val client: Web3j,
+    private val contractAddress: String,
+    private val objectMapper: ObjectMapper
+) {
+    private val logger: Logger = LoggerFactory.getLogger(Contract::class.java)
+
+    @Async
+    fun eventFlowable(): Flowable<Unit> = EthFilter(
+        DefaultBlockParameterName.LATEST,
+        DefaultBlockParameterName.LATEST,
+        contractAddress
+    ).let {
+        client.ethLogFlowable(it).map { log -> processEvent(log) }
+    }
+
+    private fun processEvent(log: Log) {
+        logger.info(
+            "Event received -> LogIndex = {}, TxHash = {}",
+            log.logIndex,
+            log.transactionHash
+        )
+
+        objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(log).let {
+            logger.info(it)
+        }
+    }
+}
