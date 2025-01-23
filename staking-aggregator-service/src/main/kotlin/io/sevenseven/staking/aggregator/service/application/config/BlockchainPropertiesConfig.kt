89e82451ff6414f9ae6832c7c9f57e93c6f23fbb
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2025-01-23 20:06:29 -0300

    feat: create a dynamic configuration to be possible only add new node and restart the application then new node will start to listening

---
+++ staking-aggregator-service/src/main/kotlin/io/sevenseven/staking/aggregator/service/application/config/BlockchainPropertiesConfig.kt
@@ -0,0 +1,30 @@
+package io.sevenseven.staking.aggregator.service.application.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+import java.io.Serializable
+
+@Configuration
+@EnableConfigurationProperties(
+    value = [
+        BlockchainNodeConfig::class
+    ]
+)
+class BlockchainPropertiesConfig
+
+@ConfigurationProperties(prefix = "app.blockchain")
+data class BlockchainNodeConfig(
+    val nodes: List<NodeConfig>,
+) : Serializable
+
+data class NodeConfig(
+    val url: String,
+    val apiKey: String,
+    val pools: List<String>,
+) : Serializable {
+
+    fun web3j(): Web3j = Web3j.build(HttpService("$url/$apiKey"))
+}
